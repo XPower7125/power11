df9a254bb18cbad9c9cceaed3b5b7d5580c41599
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2023-05-19 18:51:24 +0200

    repo update

---
+++ Configuration/removeEdge.bat
@@ -0,0 +1,5 @@
+taskkill /f /im msedge.exe
+taskkill /f /im msedge.exe
+taskkill /f /im msedge.exe
+cd %PROGRAMFILES(X86)%\Microsoft\Edge\Application
+setup.exe --uninstall --system-level --verbose-logging --force-uninstall
